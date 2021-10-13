ae76e13adbb219205131880a289475baa1ad7344
leatrix <58888415+leatrix@users.noreply.github.com>
2021-10-13 04:30:55 +0100

    2.5.59

--- Leatrix_Sounds.lua
+++ Leatrix_Sounds.lua
@@ -1,6 +1,6 @@
 ﻿
 	----------------------------------------------------------------------
-	-- Leatrix Sounds 2.5.57 (23rd September 2021)
+	-- Leatrix Sounds 2.5.59 (13th October 2021)
 	----------------------------------------------------------------------
 
 	--  Create global table
@@ -10,7 +10,7 @@
 	local LeaSoundsLC, LeaSoundsCB, LeaDropList = {}, {}, {}
 
 	-- Version
-	LeaSoundsLC["AddonVer"] = "2.5.57"
+	LeaSoundsLC["AddonVer"] = "2.5.59"
 	LeaSoundsLC["RestartReq"] = nil
 
 	-- Get locale table
--- Leatrix_Sounds_Kits.lua
+++ Leatrix_Sounds_Kits.lua
@@ -1,15 +1,15 @@
 
 	----------------------------------------------------------------------
 	-- Leatrix Sounds Sound Kit Listing
-	-- wow.tools/dbc/api/export/?name=soundkitname&build=2.5.2.40260
-	-- wow.tools/dbc/api/export/?name=soundkit&build=2.5.2.40260
+	-- wow.tools/dbc/api/export/?name=soundkitname&build=2.5.2.40488
+	-- wow.tools/dbc/api/export/?name=soundkit&build=2.5.2.40488
 	----------------------------------------------------------------------
 
 	local void, Leatrix_Sounds = ...
 	local L = Leatrix_Sounds.L
 
-	Leatrix_Sounds["SoundKitVersion"] = "2.5.2.40260"
-	Leatrix_Sounds["SoundKitNameVersion"] = "2.5.2.40260"
+	Leatrix_Sounds["SoundKitVersion"] = "2.5.2.40488"
+	Leatrix_Sounds["SoundKitNameVersion"] = "2.5.2.40488"
 
 	Leatrix_Sounds["Music"] = {
 
