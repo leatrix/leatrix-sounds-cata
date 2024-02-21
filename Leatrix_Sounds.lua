fa3a9c2ad880ff5dda0d878d8236bf6bcff54c5b
leatrix <support@leatrix.com>
2024-02-21 21:21:36 +0000

    3.0.180

--- Leatrix_Sounds.lua
+++ Leatrix_Sounds.lua
@@ -1,6 +1,6 @@
 ﻿
 	----------------------------------------------------------------------
-	-- Leatrix Sounds 3.0.180.alpha.1 (14th January 2024)
+	-- Leatrix Sounds 3.0.180 (21st January 2024)
 	----------------------------------------------------------------------
 
 	--  Create global table
@@ -10,7 +10,7 @@
 	local LeaSoundsLC, LeaSoundsCB = {}, {}
 
 	-- Version
-	LeaSoundsLC["AddonVer"] = "3.0.180.alpha.1"
+	LeaSoundsLC["AddonVer"] = "3.0.180"
 
 	-- Get locale table
 	local void, Leatrix_Sounds = ...
